5fb1202691a006f83b9ba5f01213ae3d3b430a38
Apollodev <apolloskype0@gmail.com>
2022-09-07 09:20:40 +0700

    update config/db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,20 @@
+// module.exports = {
+//     HOST: "localhost",
+//     PORT: '5432',
+//     USER: "postgres",
+//     PASSWORD: "postgres",
+//     DB: "iotim",
+//     dialect: "postgres",
+//     timezone: "Europe/London",  // this means UTC timezone
+//     local_timezone: "Asia/Singapore", 
+//     ssl: false,
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
 module.exports = {
     HOST: "private-app-82ccd341-8d5f-42fb-a861-ba64cd35858b-do-user-103446.b.db.ondigitalocean.com",
     PORT: '25060',
